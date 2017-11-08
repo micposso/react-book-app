1b0c80c0664c749d58ade199eadbf2442d2c42c5
mposso7 <micposso@gmail.com>
2017-11-07 23:54:12 -0500

    clean code

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import MdBook from 'react-icons/lib/md/book';
-import './css/App.css';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <MdBook className="App-logo" />
-          <h2>My Books App</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            div className = "App-header" >
+            <
+            MdBook className = "App-logo" / >
+            <
+            h2 > My Books App < /h2> <
+            /div> <
+            p className = "App-intro" >
+            To get started, edit < code > src / App.js < /code> and save to reload. <
+            /p> <
+            /div>
+        );
+    }
 }
 
 export default App;
