8c62fd4c8d359d9f5354a311689b27c0b577e3d0
Michael Posso <mposso@docero.com>
2017-11-10 16:27:45 -0500

    remove console logs

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -67,7 +67,7 @@ class BooksApp extends React.Component {
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                           <div className="book-shelf-changer">
-                            <select onClick={(e, b, c) => {
+                            <select onClick={(e) => {
                               e.persist();
                             }}>
                               <option value="none" disabled>Move to...</option>
@@ -101,7 +101,7 @@ class BooksApp extends React.Component {
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                           <div className="book-shelf-changer">
-                            <select onClick={(e, b, c) => {
+                            <select onClick={(e) => {
                               e.persist();
                             }}>
                               <option value="none" disabled>Move to...</option>
@@ -134,11 +134,8 @@ class BooksApp extends React.Component {
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                           <div className="book-shelf-changer">
-                            <select onClick={(e, b, c) => {
+                            <select onClick={(e) => {
                               e.persist();
-                              console.log('vallllll  e', e);
-                              console.log('vallllll  b', b);
-                              console.log('vallllll  c', c);
                             }}>
                               <option value="none" disabled>Move to...</option>
                               <option value="currentlyReading">Currently Reading</option>
