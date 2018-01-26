a2cb7e65e2e810e2eadf5f891f44fd8518c441d0
mposso7 <micposso@gmail.com>
2018-01-26 13:29:20 -0500

    moved function on select element to a method in the class

--- book-app/src/components/SearchComponent.js
+++ book-app/src/components/SearchComponent.js
@@ -16,6 +16,16 @@ class SearchComponent extends Component{
         })
     }
 
+    changeShelve = (event) => {
+        event.persist();
+        console.log(event.target.value);
+        this.setState({books: this.props.bookList.filter(book => book.title !== event.target.name)}, () => {
+        let newState = {};
+        newState[event.target.value] = this.props.bookList[event.target.value];
+        this.setState(newState);
+        });
+    }
+
     render() {
         let showBooks
         if (this.state.searchQuery) {
@@ -55,14 +65,7 @@ class SearchComponent extends Component{
                         <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                             <div className="book-shelf-changer">
-                            <select name={book.title} onChange={(event) => {
-                                event.persist();
-                                this.setState({books: this.props.bookList.filter(book => book.title !== event.target.name)}, () => {
-                                let newState = {};
-                                newState[event.target.value] = this.props.bookList[event.target.value];
-                                this.setState(newState);
-                                });
-                            }}>
+                            <select name={book.title} onChange={this.changeShelve.bind(this)}>
                                 <option value="none" disabled>Move to...</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="currentlyReading">Currently Reading</option>
