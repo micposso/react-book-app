855c178a6358dddbb0dd08916f3e8f2ba33a6c7b
Michael Posso <micposso@gmail.com>
2018-02-14 23:52:09 -0500

    seach component updates

--- src/components/BookShelfComponent.js
+++ src/components/BookShelfComponent.js
@@ -11,20 +11,20 @@ class BookShelfComponent extends Component{
                         this.props.books.map((book, index) => 
                         <li key={index}>
                             <div className="book">
-                            <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
-                                <div className="book-shelf-changer">
-                                <select value={book.shelf} onChange={(e) => this.props.handleChangeShelve(book, e.target.value)}>
-                                    <option value="none">Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
+                                    <div className="book-shelf-changer">
+                                    <select value={book.shelf} onChange={(e) => this.props.handleChangeShelve(book, e.target.value)}>
+                                        <option value="none">Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            {/* <div className="book-authors">{book.authors.join(', ')}</div> */}
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors}</div>
                             </div>
                         </li>
                     )
