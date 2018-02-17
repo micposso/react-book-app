3c7862e6eff10cb5e987b7030c042813c5d4d4d4
mposso7 <micposso@gmail.com>
2018-02-16 23:31:42 -0500

    add authors to shelf component

--- src/components/BookShelfComponent.js
+++ src/components/BookShelfComponent.js
@@ -24,7 +24,7 @@ class BookShelfComponent extends Component{
                                 </div>
                             </div>
                             <div className="book-title">{book.title}</div>
-                            {/* <div className="book-authors">{book.authors.join(', ')}</div> */}
+                            <div className="book-authors">{book.authors}</div>
                             </div>
                         </li>
                     )
