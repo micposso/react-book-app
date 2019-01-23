29ca1bf91b9ebc07d37f8ab6a5bd9cb9f06ed1fd
Posso <micposso@gmail:.com>
2019-01-23 17:55:02 -0500

    converted component to stateles component and passed shelf propr to bookcomponent

--- src/components/BookShelfComponent.js
+++ src/components/BookShelfComponent.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 import BookComponent from './BookComponent.js'
 
-class BookShelfComponent extends Component{
-    render(){
-        return(
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.headline}</h2>
-                <div className="bookshelf-books">        
-                    <ol className="books-grid">
-                        <BookComponent books={this.props.books} handleChangeShelve={this.props.handleChangeShelve} />
-                    </ol>
-                </div>
-          </div>
-        )
-    }
+const BookShelfComponent = (props) => {
+    return(
+        <div className="bookshelf">
+        <h2 className="bookshelf-title">{props.headline}</h2>
+        <div className="bookshelf-books">        
+            <ol className="books-grid">
+                <BookComponent books={props.books} handleChangeShelve={props.handleChangeShelve} />
+            </ol>
+        </div>
+    </div>
+    )
 }
 
 
